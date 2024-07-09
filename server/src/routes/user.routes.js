9a36187c13bc7de190729a83ef71ff72bb112c6a
Shree2124 <shreealasande@gmail.com>
2024-07-09 12:18:43 +0530

    server deleted

--- server/src/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { upload } from "../middlewares/multer.middleware.js"
-import { register } from "../controllers/user.controllers.js"
-
-const Router = express.Router()
-
-Router.route("/register").post(upload.single("avatar"),register)
